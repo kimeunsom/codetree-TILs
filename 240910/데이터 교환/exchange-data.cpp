ac23c9edb7ea153c24b85511902f94beb2ff3323
cotton <72211340+kimeunsom@users.noreply.github.com>
2024-09-10 23:28:58 +0900

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240910/데이터 교환/exchange-data.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a=5;
+    int b=6;
+    int c=7;
+    int temp1, temp2;
+    temp1 = b;
+    temp2 = c;
+    b=a;
+    c=temp1;
+    a=temp2;
+
+    cout << a << "\n"  << b<< "\n" << c;
+    return 0;
+}
