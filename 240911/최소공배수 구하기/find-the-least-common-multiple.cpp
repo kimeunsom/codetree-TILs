efc4ffa8208336b5c7173c02cac023f4e3b1ea0b
cotton <72211340+kimeunsom@users.noreply.github.com>
2024-09-11 12:30:33 +0900

    Title: 최소공배수 구하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240911/최소공배수 구하기/find-the-least-common-multiple.cpp
+++ 240911/최소공배수 구하기/find-the-least-common-multiple.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void getLcd(int n, int m) {
     int gcd = 1;
-    for(int i=1; i<min(n,m); i++) {
+    for(int i=1; i<=min(n,m); i++) {
         if(n%i==0 && m%i==0) gcd = i;
     }
     cout << n*m/gcd;
