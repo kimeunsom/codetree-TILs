987bbf3ae180d2b2d294b4ea89f19da25343775f
cotton <72211340+kimeunsom@users.noreply.github.com>
2024-09-11 12:28:07 +0900

    Title: 최소공배수 구하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240911/최소공배수 구하기/find-the-least-common-multiple.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void getLcd(int n, int m) {
+    int gcd = 1;
+    for(int i=1; i<min(n,m); i++) {
+        if(n%i==0 && m%i==0) gcd = i;
+    }
+    cout << n*m/gcd;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n,m;
+    cin >> n >> m;
+
+    getLcd(n,m);
+    return 0;
+}
