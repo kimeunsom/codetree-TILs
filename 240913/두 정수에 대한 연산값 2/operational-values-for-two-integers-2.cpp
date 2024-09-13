dcf21cefc8b6ca40f76395454957433fc51c200a
cotton <72211340+kimeunsom@users.noreply.github.com>
2024-09-13 11:34:33 +0900

    Title: 두 정수에 대한 연산값 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240913/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void solution(int& a, int& b) {
+    if(a>b) {
+        b+=10;
+        a*=2;
+    } else {
+        a+=10;
+        b*=2;
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> a >> b;
+
+    solution(a, b);
+
+    cout << a << " " << b ;
+
+    return 0;
+}
